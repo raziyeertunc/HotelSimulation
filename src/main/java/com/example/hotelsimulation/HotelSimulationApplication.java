c8e7bd053f8d51461764afd0475f978ab80756f2
raziyeertunc <87854792+raziyeertunc@users.noreply.github.com>
2022-10-29 17:59:44 +0300

    Initial commit

---
+++ src/main/java/com/example/hotelsimulation/HotelSimulationApplication.java
@@ -0,0 +1,13 @@
+package com.example.hotelsimulation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelSimulationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelSimulationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/hotelsimulation/HotelSimulationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hotelsimulation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelSimulationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
